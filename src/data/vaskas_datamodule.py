1cbf046d61a47f0968893b35e4ca8b944764a819
PrathDeshmukh <143096196+PrathDeshmukh@users.noreply.github.com>
2024-03-13 11:46:30 +0100

    Added vaskas datamodule file

--- src/data/vaskas_datamodule.py
+++ src/data/vaskas_datamodule.py
@@ -2,7 +2,14 @@ import os.path
 from typing import Optional
 
 from src.data.ase_datamodule import ASEDataModule
-from src.data.components.datasets
+from src.data.components.datasets.vaskas import VaskasDataset
+
 class VaskasDataModule(ASEDataModule):
   dataset_cls = VaskasDataset
+  dataset: Optional[VaskasDataset]
+  splits: Optional[dict[str, VaskasDataset]]
+  
+  def prepare_data(self) -> None:
+    if not os.path.exists(self.hparams.db_path):
+      VaskasDataset(self.hparams.db_path, download=True, in_memory=False)
   
