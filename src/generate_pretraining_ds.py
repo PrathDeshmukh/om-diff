56dab46d61f0a3a8223d9eb9904c52618b42a7a1
PrathDeshmukh <143096196+PrathDeshmukh@users.noreply.github.com>
2024-04-22 17:04:13 +0200

    Added additional scripts for tmqm pre-training

--- src/generate_pretraining_ds.py
+++ src/generate_pretraining_ds.py
@@ -11,6 +11,12 @@ all_xyz = glob.glob(os.path.join(path_tmqm, "*.xyz"))
 with connect(db_path, append=False) as db:
   for xyz in all_xyz:
     atoms = read(xyz)
-    del atoms[[atom.index for atom in atoms if atom.symbol == 'H']]
-    db.write(atoms)
+    if len(atoms) < 70:
+      formula = atoms.get_chemical_formula()
+      with open(xyz, 'r') as file:
+        lines = file.readlines()
+      mol_info = lines[1]
+      if int(mol_info[-2]) == 4 and all(element not in formula for element in ['B', 'Si', 'Se']):
+        del atoms[[atom.index for atom in atoms if atom.symbol == 'H']]
+        db.write(atoms)
   
