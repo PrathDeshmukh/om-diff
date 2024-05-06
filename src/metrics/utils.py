9a0d94399e75d855f2ca1449a756319d943dd2d3
PrathDeshmukh <143096196+PrathDeshmukh@users.noreply.github.com>
2024-05-06 12:33:30 +0200

    Added screening pipeline

--- src/metrics/utils.py
+++ src/metrics/utils.py
@@ -11,24 +11,26 @@ def atoms_to_xyz_str(atoms: ase.Atoms):
   return f.getvalue()
 
 
-
-def remove_h2(self) -> None:
-  atoms, charge, xyz = xm.read_xyz_file(self.sample)
+def remove_h2(sample):
+  at_list = [int(at) for at in sample.get_atomic_numbers()]
+  xyz = sample.get_positions()
   
-  ase_atom_H2 = Atoms(numbers=atoms, positions=xyz, pbc=False)
+  ase_atom_H2 = sample
   ase_atom = ase_atom_H2.copy()
   
-  AC, mol = xm.xyz2AC(atoms, xyz, charge)
+  AC, mol = xm.xyz2AC(at_list, xyz, charge=0)
   
-  metal_connectivity = AC[atoms.index(77)]
-  H_pos = [xyz[i] for i in range(len(metal_connectivity)) if metal_connectivity[i] == 1 and atoms[i] == 1]
+  metal_connectivity = AC[at_list.index(77)]
+  H_pos = [list(xyz[i]) for i in range(len(metal_connectivity)) if metal_connectivity[i] == 1 and at_list[i] == 1]
+  H_index = [atom.index for atom in ase_atom if atom.position.tolist() in H_pos]
+  
   assert len(H_pos) == 2, f"No. of H's is {len(H_pos)}"
-  del ase_atom[[atom.index for atom in ase_atom if atom.position.tolist() in H_pos]]
+  del ase_atom[H_index]
   
-  return ase_atom
+  return ase_atom, H_index
 
 
-def write_basisfile(atoms, basisfile):
+def write_basisfile(atoms, basisfile, label):
   at_types = atoms.symbols.species()
   at_types.remove('Ir')
   at_list = list(at_types)
@@ -39,5 +41,5 @@ def write_basisfile(atoms, basisfile):
   
   lines[3] = " ".join(at_list) + " " + "0" + "\n"
   
-  with open(basisfile, 'w') as f:
+  with open(f"basisfile_{label}", 'w') as f:
     f.writelines(lines)
--- src/vaskas_pipline.py
+++ src/vaskas_pipline.py
@@ -1,95 +1,285 @@
-from os.path import join, splitext, basename
-from xyz2mol import xyz2mol as xm
-from ase import Atoms
-from openbabel import openbabel
-from ase.io import write, read
-from ase.db import connect
+import glob, os
+import argparse
+import ase
+import submitit
+from ase.io import write, read, iread
+
 from ase.calculators.gaussian import Gaussian, GaussianOptimizer
-from rdkit.Chem import MolToSmiles, MolFromSmiles, DetectChemistryProblems
-from src.metrics.utils import atoms_to_xyz_str, remove_h2
+from rdkit.Chem import DetectChemistryProblems, rdDetermineBonds
+from rdkit import Chem
+from src.metrics.utils import remove_h2, atoms_to_xyz_str, write_basisfile
 
-h2_energy = 0
+basis = r'/home/energy/s222491/TS_basis.gbs'
+
+def parse_cmd():
+  parser = argparse.ArgumentParser(description="Checking validty and novelty of samples and running DFT calculations")
+  
+  parser.add_argument(
+    "--samples_path",
+    type=str,
+    required=True,
+  )
+  
+  parser.add_argument(
+    "--barrier_criteria",
+    type=float,
+    required=True,
+  )
+  return parser.parse_args()
 
 
-class VaskasPipeline:
+cmd_args = parse_cmd()
+samples_path = os.path.join(cmd_args.samples_path, 'samples.xyz')
+barrier_criteria = cmd_args.barrier_path
+predictions = os.path.join(cmd_args.samples_path, 'predictions.pt')
+
+
+class RunDFT:
   scratch = r'/home/scratch3/s222491/'
   db_path = r'/home/scratch3/s222491/vaskas/vaskas.db'
   
+  def __init__(self, ts: ase.Atoms, label: str):
+    if not isinstance(ts, ase.Atoms):
+      raise ValueError("Invalid input type.")
+    
+    self.ts = ts
+    self.H_index = None
+    self.label = label
+    write_basisfile(self.ts, basisfile=basis, label=self.label)
   
-  def __init__(self, sample):
-    self.barrier = None
-    self.ts_atoms = None
-    self.cat_xyz = None
-    self.cat_atoms = None
-    self.sample = sample
-    self.filename = splitext(basename(self.sample))[0]
+  def catOpt(self):
+    atoms, self.H_index = remove_h2(self.ts)
+    
+    calc_opt = Gaussian(mem='8GB',
+                        label=os.path.join(samples_path, self.label),
+                        xc='PBE',
+                        chk=os.path.join(samples_path, self.label, f'cat_{self.label}.chk'),
+                        basisfile=basis,
+                        mult=1,
+                        charge=0
+                        )
+    opt = GaussianOptimizer(atoms, calc_opt)
+    opt.run(fmax='tight')
+    
+    write(os.path.join(samples_path, self.label, f'cat_{self.label}.xyz'), atoms)
+    energy = atoms.get_potential_energy()
+    
+    return energy
   
-  def validity_check(self):
+  def tsOpt(self, atoms):
+    calc_opt = Gaussian(mem='8GB',
+                        label=os.path.join(samples_path, self.label),
+                        xc='PBE',
+                        chk=os.path.join(samples_path, self.label, f'ts_{self.label}.chk'),
+                        basisfile=basis,
+                        mult=1,
+                        charge=0,
+                        extra="nosymm EmpiricalDispersion=GD3 pseudo=read"
+                        )
+    opt = GaussianOptimizer(atoms, calc_opt)
+    opt.run(opt='calcfc,ts')
+    
+    energy = atoms.get_potential_energy()
+    write(os.path.join(samples_path, self.label, f'ts_{self.label}.xyz'), atoms)
+    return energy
+  
+  def tsFreeze(self):
+    atoms = self.ts
+    addsec = f"B {self.H_index[0] + 1} {self.H_index[1] + 1} F"
+    
+    calc_opt = Gaussian(mem='8GB',
+                        label=os.path.join(samples_path, self.label),
+                        xc='PBE',
+                        chk=os.path.join(samples_path, self.label, f'ts_freeze_{self.label}.chk'),
+                        basisfile=basis,
+                        addsec=addsec,
+                        mult=1,
+                        charge=0,
+                        extra="nosymm EmpiricalDispersion=GD3 pseudo=read"
+                        )
+    opt = GaussianOptimizer(atoms, calc_opt)
+    opt.run(opt='addredun')
+    
+    energy = self.tsOpt(atoms=atoms)
+    return energy
+
+
+def compute_dft(sample, label):
+  dft = RunDFT(sample, label)
+  try:
+    cat_energy = dft.catOpt()
+  except Exception as e:
+    print(f"Exception {e} in complex no. {label}")
+    return None
+  else:
     try:
-      atom = read(self.sample)
-      clean_atom = remove_h2(atom)
+      ts_energy = dft.tsOpt(sample)
+      barrier = ts_energy - cat_energy - H_energy
+      return barrier
+    except:
+      try:
+        ts_energy = dft.tsFreeze()
+        barrier = ts_energy - cat_energy - H_energy
+        return label, barrier
+      
+      except:
+        print(f"No transition state for sample {label}")
+        return None
+
+
+class ValidityCheck:
+  def __init__(self, ts: ase.Atoms):
+    if not isinstance(ts, ase.Atoms):
+      print("Invalid input type.")
+    self.charges = [-2, -1, 0, 1, 2]
+    self.ts = ts
+  
+  def checkvalidity(self):
+    try:
+      atom = self.ts
+      clean_atom, H_ind = remove_h2(atom)
       natoms = len(clean_atom)
       for j, num in enumerate(reversed(clean_atom.numbers)):
         if num == 77:
           clean_atom.pop(natoms - j - 1)
-      xyz = atoms_to_xyz_str(clean_atom)
-      obConversion = openbabel.OBConversion()
-      obConversion.SetInFormat('xyz')
       
-      mol = openbabel.OBMol()
-      obConversion.ReadString(mol, xyz)
-      mol.MakeDativeBonds()
+      As_index = [id for id, atom in enumerate(clean_atom) if atom.symbol == 'As']
+      for id in As_index:
+        clean_atom[id].symbol = 'P'
       
-      obConversion.SetOutFormat("smi")
-      smiles = obConversion.WriteString(mol)
-      
-      mol_rdk = MolFromSmiles(smiles)
-      probs = DetectChemistryProblems(mol_rdk)
-      
-      if len(probs) > 0:
-        return self.sample
+      ind_samples = []
+      for chg in self.charges:
+        try:
+          xyz = atoms_to_xyz_str(clean_atom)
+          raw_mol = Chem.MolFromXYZBlock(xyz)
+          mol = Chem.Mol(raw_mol)
+          rdDetermineBonds.DetermineBonds(
+            mol,
+            charge=chg,
+          )
+          mol.UpdatePropertyCache(strict=False)
+          probs = DetectChemistryProblems(mol)
+          smi = Chem.MolToSmiles(mol)
+          frags = smi.split(".")
+          if len(probs) == 0 and len(frags) <= 4:
+            ind_samples.append(smi)
+        except Exception as e:
+          print(e)
+          continue
+      if len(ind_samples) > 0:
+        return self.ts
     
     except Exception as e:
-      print(f"Exception {e} in samples {self.sample}")
+      print(f"Exception {e} in samples {self.ts}")  # Replace self.ts with a label/identifier
       return None
-    
+
+
+ds_path = r"/home/scratch3/s222491/vaskas/coordinates_complex"
+all_ds_xyz = glob.glob(os.path.join(ds_path, "*.xyz"))
+all_ds_atoms = [read(xyz) for xyz in all_ds_xyz]
+ds_atoms_cleaned = []
+for a in all_ds_atoms:
+  del a[[1, 2]]
+  ds_atoms_cleaned.append(a)
+
+
+def atoms2fp(clean_atom):
+  natoms = len(clean_atom)
+  for j, num in enumerate(reversed(clean_atom.numbers)):
+    if num == 77:
+      clean_atom.pop(natoms - j - 1)
   
-  def dft(self) -> None:
-    calc = Gaussian(xc='PBE',
-                    basis='def2SVP',
-                    scf='qc',
-                    command='g16 < PREFIX.com > PREFIX.log',
-                    mult=1,
-                    charge=0
-                    )
-    calc.set_label(join(VaskasPipeline.scratch, f'{self.filename}_cat.xyz'))
-    self.cat_atoms.calc = calc
-    
-    calc.set_label(join(VaskasPipeline.scratch, f'{self.filename}_ts.xyz'))
-    self.ts_atoms.calc = calc
-    
-    opt = GaussianOptimizer(self.cat_atoms, calc)
-    opt.run(fmax='tight', steps=100)
-    
-    #Implement code for TS calculation here
-    
-    cat_energy = self.cat_atoms.get_potential_energy()
-    ts_energy = self.ts_atoms.get_potential_energy()
-    self.barrier = ts_energy - cat_energy - h2_energy
-    
-    write(join(VaskasPipeline.scratch, 'DFT_cat', f'{self.filename}.xyz'), self.cat_atoms)
-    write(join(VaskasPipeline.scratch, 'DFT_ts', f'{self.filename}.xyz'), self.ts_atoms)
-    
-  @staticmethod
-  def xyz2smi(xyz):
-    atom, charge, coords = xm.read_xyz_file(xyz)
-    mol = xm.xyz2mol(atoms=atom, charge=charge, coordinates=coords)
-    smiles = MolToSmiles(mol[0])
-    
-    return smiles
+  # Replace As with P because rdkit does not work with As
+  As_index = [id for id, atom in enumerate(clean_atom) if atom.symbol == 'As']
+  for l in As_index:
+    clean_atom[l].symbol = 'P'
+  fps_arr = []
+  charges = [-2, -1, 0, 1, 2]
   
-  def add_to_training(self) -> None:
-    smi = self.xyz2smi(self.cat_xyz)
+  for chg in charges:
+    try:
+      xyz = atoms_to_xyz_str(clean_atom)
+      
+      raw_mol = Chem.MolFromXYZBlock(xyz)
+      mol = Chem.Mol(raw_mol)
+      rdDetermineBonds.DetermineBonds(
+        mol,
+        charge=chg,
+      )
+      fp = Chem.RDKFingerprint(mol)
+      if fp:
+        fps_arr.append(fp)
+    except:
+      pass
+  
+  return fps_arr
+
+
+all_ds_fps = [atoms2fp(ds_atoms) for ds_atoms in ds_atoms_cleaned]
+
+
+def novelty(sample):
+  sim_arr = []
+  try:
+    clean_atoms, H_ind = remove_h2(sample)
+    gen_fps = atoms2fp(clean_atoms)
     
-    with connect(VaskasPipeline.db_path, append=True) as db:
-      db.write(self.cat_atoms, key_value_pairs = {'barrier': self.barrier, 'smiles': smi})
+    for fps in gen_fps:
+      for j, ds_fps in enumerate(all_ds_fps):
+        try:
+          for ds_fp in ds_fps:
+            sim = Chem.DataStructs.TanimotoSimilarity(ds_fp, fps)
+            if sim >= 0.90:
+              print(f"Similarity {sim} for sample {i} with ds sample {j}")
+              sim_arr.append([sim, j])
+        except:
+          pass
+  except Exception as e:
+    print(f"Exception {e} in sample {i}")
+  
+  if len(sim_arr) > 0:
+    return None
+  else:
+    return sample
+
+
+all_samples = iread(samples_path)
+H_energy = -1.1389  # Write H energy
+
+novel_samples = []
+valid_samples = []
+labels = []
+
+for i, sample in enumerate(all_samples):
+  novel_sample = novelty(sample)
+  if novel_sample:
+    novel_samples.append(sample)
+    val_chk = ValidityCheck(novel_sample)
+    valid_sample = val_chk.checkvalidity()
+    if valid_sample:
+      valid_samples.append(valid_sample)
+      labels.append(i)
+
+print(f"No. of novel samples: {len(novel_samples)}")
+print(f"No. of valid samples: {len(valid_samples)}")
+print(f"Total number of samples: {i}")
+
+all_samples_list = list(all_samples)
+preds = torch.load(predictions)
+samples_for_dft = [all_samples_list[i] for i in labels if preds['barrier'][i] <= barrier_criteria + 1]
+
+executor = submitit.AutoExecutor(folder="sublogs/")
+
+if executor._executor.__class__.__name__ == 'SlurmExecutor':
+  executor.update_parameters(
+    slurm_partition="xeon24",
+    cpus_per_task=24,
+    tasks_per_node=1,
+    slurm_nodes=1,
+    slurm_time="2-00:00:00",
+  )
+
+jobs = executor.map_array(compute_dft, samples_for_dft,
+                          labels)  # submit_fn = function that runs DFT calculation, list_of_params=list of atoms object
+
+print(jobs.result())
